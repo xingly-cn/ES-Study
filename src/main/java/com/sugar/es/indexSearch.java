4a425d4f0f8fdfb5c673c999f89f59743296faa2
xingly-cn <987599519@qq.com>
2022-01-09 16:48:04 +0800

    查询索引

---
+++ src/main/java/com/sugar/es/indexSearch.java
@@ -0,0 +1,52 @@
+package com.sugar.es;
+
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.client.indices.GetIndexResponse;
+
+public class indexSearch {
+    private final static String host = "es-1eq9og57.public.tencentelasticsearch.com";
+    private final static int port = 9200;
+    private final static String scheme  = "https";
+
+    private final static String UserName = "elastic";
+    private final static String PassWord = "XNXxnx520@";
+
+    public static void main(String[] args) throws Exception{
+        // 认证
+        BasicCredentialsProvider provider = new BasicCredentialsProvider();
+        provider.setCredentials(AuthScope.ANY,new UsernamePasswordCredentials(UserName,PassWord));
+
+        // 创建客户端
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost(host,port,scheme)
+                ).setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpAsyncClientBuilder) {
+                        httpAsyncClientBuilder.disableAuthCaching();
+                        return httpAsyncClientBuilder.setDefaultCredentialsProvider(provider);
+                    }
+                })
+        );
+
+        // 查询索引
+        GetIndexRequest request = new GetIndexRequest("role2");
+        GetIndexResponse response = client.indices().get(request, RequestOptions.DEFAULT);
+
+        System.out.println("查询Aliases：" + response.getAliases());
+        System.out.println("查询Mapping：" + response.getMappings());
+        System.out.println("查询Settings：" + response.getSettings());
+
+
+        client.close();
+
+    }
+}
