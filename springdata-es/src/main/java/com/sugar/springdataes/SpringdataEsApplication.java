e877f67122ad052e975faa6fed32f5a66a31854e
xingly-cn <987599519@qq.com>
2022-01-09 20:50:12 +0800

    init

---
+++ springdata-es/src/main/java/com/sugar/springdataes/SpringdataEsApplication.java
@@ -0,0 +1,13 @@
+package com.sugar.springdataes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataEsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdataEsApplication.class, args);
+    }
+
+}
---
+++ springdata-es/src/test/java/com/sugar/springdataes/SpringdataEsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sugar.springdataes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdataEsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
